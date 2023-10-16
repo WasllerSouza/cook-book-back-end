ba40f3ab38075eecec08f4b66b92d47d8319cc8d
Wasller Souza <souzawasller@gmail.com>
2023-10-15 21:14:29 -0300

    feat: criando migrations para tabela usuario no banco de dados

--- src/CookBook.Api/Program.cs
+++ src/CookBook.Api/Program.cs
@@ -1,3 +1,8 @@
+using CookBook.Domain.Extension;
+using CookBook.Infrastructure;
+using CookBook.Infrastructure.Migrations;
+using FluentMigrator.Runner;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddRepository(builder.Configuration);
 
 var app = builder.Build();
 
@@ -22,4 +29,16 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+UpdateInternalDatabase();
+
 app.Run();
+
+void UpdateInternalDatabase()
+{
+    var connection = builder.Configuration.getConnectionDatabase();
+    var nameDatabase = builder.Configuration.getNameDatabase();
+
+    Database.CreateDatabase(connection, nameDatabase);
+
+    app.MigrateDataBase();
+}
---
+++ src/CookBook.Infrastructure/Migrations/VersionBase.cs
@@ -0,0 +1,14 @@
+﻿using FluentMigrator.Builders.Create.Table;
+
+namespace CookBook.Infrastructure.Migrations;
+
+public static class VersionBase
+{
+    public static ICreateTableColumnOptionOrWithColumnSyntax InsertColumnBase(ICreateTableWithColumnOrSchemaOrDescriptionSyntax table)
+    {
+        return table
+            .WithColumn("Id").AsInt64().PrimaryKey().Identity()
+            .WithColumn("CreatedAt").AsDateTime().NotNullable()
+            .WithColumn("LastUpdate").AsDateTime().NotNullable();
+    }
+}
