1e3e4096b87355579ad10e45128b06359b531c3f
Wasller Souza <souzawasller@gmail.com>
2023-10-29 21:33:30 -0300

    Implementação da entidade usuario e entidade base

--- src/CookBook.Domain/Entity/EntityBase.cs
+++ src/CookBook.Domain/Entity/EntityBase.cs
@@ -1,14 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CookBook.Domain.Entity;
+﻿namespace CookBook.Domain.Entity;
 
 public class EntityBase
 {
     public long Id { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime lastUpdate { get; set; }
+
+    public void prePersist()
+    {
+        CreatedAt = DateTime.UtcNow;
+        lastUpdate = DateTime.UtcNow;
+    }
+    public void preUpdate()
+    {
+        lastUpdate = DateTime.UtcNow;
+    }
 }
--- src/CookBook.Domain/Entity/Usuario.cs
+++ src/CookBook.Domain/Entity/Usuario.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CookBook.Domain.Entity;
+﻿namespace CookBook.Domain.Entity;
 
 public class Usuario : EntityBase
 {
