850a5083ea4f4256bedb7618e0c6a89e34dc0109
Wasller Souza <souzawasller@gmail.com>
2023-12-17 01:39:47 -0300

    Feat: Implementar regra de negocio e validators para cadastro e validação das receitas

---
+++ src/CookBook.Application/UseCases/Revenue/Register/IRevenueRegisterUseCase.cs
@@ -0,0 +1,8 @@
+﻿using CookBook.Communication.Request;
+using CookBook.Communication.Response;
+
+namespace CookBook.Application.UseCases.Revenue.Register;
+public interface IRevenueRegisterUseCase
+{
+    Task<GenericResponse<dynamic>> Execute(RevenueRequest request);
+}
