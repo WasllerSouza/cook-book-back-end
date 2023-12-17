0564c88e92f5b760d6f0a8fefbf1416acf306310
Wasller Souza <souzawasller@gmail.com>
2023-12-17 01:42:05 -0300

    Feat: Implementando communication request e response para receitas

---
+++ src/Shared/CookBook.Communication/Response/IngredientResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace CookBook.Communication.Response;
+public class IngredientResponse
+{
+    public Guid Id { get; set; }
+    public string Produto { get; set; }
+    public string Quantidade { get; set; }
+}
---
+++ src/Shared/CookBook.Communication/Response/RevenueResponse.cs
@@ -0,0 +1,16 @@
+﻿using CookBook.Communication.Enum;
+
+namespace CookBook.Communication.Response;
+public class RevenueResponse
+{
+    public Guid Id { get; set; }
+    public string Titulo { get; set; }
+    public Categoria Categoria { get; set; }
+    public string ModoPreparo { get; set; }
+    public List<IngredientResponse> Ingredientes { get; set; }
+
+    public RevenueResponse()
+    {
+        Ingredientes = new();
+    }
+}
