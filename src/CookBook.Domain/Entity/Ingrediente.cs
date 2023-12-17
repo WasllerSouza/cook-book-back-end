cc021a59e381f4dfa80e92bccca88fcab4ce695b
Wasller Souza <souzawasller@gmail.com>
2023-12-16 21:45:38 -0300

    feat: Implementar entidade receita e ingrediente

---
+++ src/CookBook.Domain/Entity/Ingrediente.cs
@@ -0,0 +1,7 @@
+﻿namespace CookBook.Domain.Entity;
+public class Ingrediente : EntityBase
+{
+    public string Produto { get; set; }
+    public string Quantidade { get; set; }
+    public long ReceitaId { get; set; }
+}
---
+++ src/CookBook.Domain/Entity/Receita.cs
@@ -0,0 +1,10 @@
+﻿using CookBook.Domain.Enum;
+
+namespace CookBook.Domain.Entity;
+public class Receita : EntityBase
+{ 
+    public String Titulo { get; set; }
+    public Categoria Categoria { get; set; }
+    public String ModoPreparo { get; set; }
+    public ICollection<Ingrediente> Ingredientes { get; set; }
+}
